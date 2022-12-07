0d0ab76ac3255c62305f99fe213dc3b449f83811
NguyenDuyHieu987 <vaicut6941@gmail.com>
2022-12-07 12:08:45 +0700

    add

--- src/components/DetailAccountForm.vue
+++ src/components/DetailAccountForm.vue
@@ -1,45 +1,60 @@
 <template>
-  <div class="input-container">
-    <strong class="title-input">Id: </strong>
-    <p>{{ $store.state.detailAccount?.id }}</p>
+  <div class="detail-account" v-if="$store.state.detailAccount">
+    <div class="input-container">
+      <strong class="title-input">Id: </strong>
+      <p>{{ $store.state.detailAccount?.id }}</p>
+    </div>
+    <div class="input-container">
+      <strong class="title-input">Name: </strong>
+      <p>{{ $store.state.detailAccount?.name }}</p>
+    </div>
+    <div class="input-container">
+      <strong class="title-input">Phone: </strong>
+      <p>{{ $store.state.detailAccount?.phone }}</p>
+    </div>
+    <div class="input-container">
+      <strong class="title-input">Iban: </strong>
+      <p>{{ $store.state.detailAccount?.iban }}</p>
+    </div>
+    <div class="input-container">
+      <strong class="title-input">Pin: </strong>
+      <p>{{ $store.state.detailAccount?.pin }}</p>
+    </div>
+    <div class="input-container">
+      <strong class="title-input">Address: </strong>
+      <p>{{ $store.state.detailAccount?.address }}</p>
+    </div>
+    <div class="input-container">
+      <strong class="title-input">Balance: </strong>
+      <p>{{ $store.state.detailAccount?.balance }}</p>
+    </div>
+    <div class="input-container">
+      <strong class="title-input">Email: </strong>
+      <p>{{ $store.state.detailAccount?.email }}</p>
+    </div>
+    <div class="input-container">
+      <strong class="title-input">Date: </strong>
+      <p>{{ $store.state.detailAccount?.date }}</p>
+    </div>
   </div>
-  <div class="input-container">
-    <strong class="title-input">Name: </strong>
-    <p>{{ $store.state.detailAccount?.name }}</p>
-  </div>
-  <div class="input-container">
-    <strong class="title-input">Phone: </strong>
-    <p>{{ $store.state.detailAccount?.phone }}</p>
-  </div>
-  <div class="input-container">
-    <strong class="title-input">Iban: </strong>
-    <p>{{ $store.state.detailAccount?.iban }}</p>
-  </div>
-  <div class="input-container">
-    <strong class="title-input">Pin: </strong>
-    <p>{{ $store.state.detailAccount?.pin }}</p>
-  </div>
-  <div class="input-container">
-    <strong class="title-input">Address: </strong>
-    <p>{{ $store.state.detailAccount?.address }}</p>
-  </div>
-  <div class="input-container">
-    <strong class="title-input">Balance: </strong>
-    <p>{{ $store.state.detailAccount?.balance }}</p>
-  </div>
-  <div class="input-container">
-    <strong class="title-input">Email: </strong>
-    <p>{{ $store.state.detailAccount?.email }}</p>
-  </div>
-  <div class="input-container">
-    <strong class="title-input">Date: </strong>
-    <p>{{ $store.state.detailAccount?.date }}</p>
+  <div class="loading-table" v-else>
+    <half-circle-spinner
+      :animation-duration="1000"
+      :size="60"
+      color="var(--header-table-color)"
+      class="loading-icon"
+    />
   </div>
 </template>
 
 <script>
+import { HalfCircleSpinner } from 'epic-spinners';
+
 export default {
   name: 'DetailAccountForm',
+  components: {
+    HalfCircleSpinner,
+  },
   props: {},
   data() {
     return {};
